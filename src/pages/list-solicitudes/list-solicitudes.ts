8b8e26a2de10dc21d1b56391b97f649ede22ffa8
NicoWeisheim <nicoweisheim@gmail.com>
2018-08-27 17:23:23 -0300

    trying accordion list

--- src/pages/list-solicitudes/list-solicitudes.ts
+++ src/pages/list-solicitudes/list-solicitudes.ts
@@ -14,19 +14,25 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ListSolicitudesPage {
 
-  solicitudes: any[] = [{}]
+  solicitudes: any[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListSolicitudesPage');
     this.solicitudes = [
-      {fechaPed: '20/01/2018', sup: 'Tomas Fanta', cantPer: '10', destino: 'San Luis', diaIngreso: '20/02/2018'},
-      {fechaPed: '20/02/2018', sup: 'Juan Perez', cantPer: '11', destino: 'Mendoza', diaIngreso: '20/03/2018'},
-      {fechaPed: '20/03/2018', sup: 'Nicolas Weisheim', cantPer: '15', destino: 'Cordoba', diaIngreso: '20/04/2018'},
-      {fechaPed: '20/04/2018', sup: 'Oscar Tabarez', cantPer: '22', destino: 'Salta', diaIngreso: '20/05/2018'},
-      {fechaPed: '20/05/2018', sup: 'Pepe Pepon', cantPer: '5', destino: 'La Pampa', diaIngreso: '20/06/2018'},
-    ]
+      {fechaPed: '20/01/2018', sup: 'Tomas Fanta', cantPer: '10', destino: 'San Luis', diaIngreso: '20/02/2018', open: false},
+      {fechaPed: '20/02/2018', sup: 'Juan Perez', cantPer: '11', destino: 'Mendoza', diaIngreso: '20/03/2018', open: false},
+      {fechaPed: '20/03/2018', sup: 'Nicolas Weisheim', cantPer: '15', destino: 'Cordoba', diaIngreso: '20/04/2018', open: false},
+      {fechaPed: '20/04/2018', sup: 'Oscar Tabarez', cantPer: '22', destino: 'Salta', diaIngreso: '20/05/2018', open: false},
+      {fechaPed: '20/05/2018', sup: 'Pepe Pepon', cantPer: '5', destino: 'La Pampa', diaIngreso: '20/06/2018', open: false},
+    ];
+    console.log(this.solicitudes);
+  }
+
+  toggleSection(i) {
+    
+    this.solicitudes[i].open = !this.solicitudes[i].open ? true : false;
   }
 
 }
